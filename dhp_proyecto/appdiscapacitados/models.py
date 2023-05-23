f250a5a6b8c70d8db945abd4ad7c693d2021d2ce
m4rtinjoel <m4rtinjl@gmail.com>
2023-05-22 21:37:09 -0600

    Intento de creación de modelos

--- dhp_proyecto/appdiscapacitados/models.py
+++ dhp_proyecto/appdiscapacitados/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True, null=False ,unique=True)
+    nombre= models.CharField(max_length=20, null=False)
+    apellido= models.CharField(max_length=20, null=False)
+    correo=models.EmailField(max_length=256, null= False)
+    estado=models.BooleanField()
