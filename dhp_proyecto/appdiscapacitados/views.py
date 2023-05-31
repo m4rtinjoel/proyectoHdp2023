9f775dc28413832983a6b938f60382b462940ff2
m4rtinjoel <m4rtinjl@gmail.com>
2023-05-30 21:59:21 -0600

    Agregar primeras vistas, configuración del propecyo

--- dhp_proyecto/appdiscapacitados/views.py
+++ dhp_proyecto/appdiscapacitados/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Publicacion(request):
+    return render(request,'publicacion.html')
+
+def Graficos(request):
+    return render(request, 'graficos.html')
