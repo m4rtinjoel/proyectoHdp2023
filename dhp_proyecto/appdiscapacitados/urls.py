73b5ed53e4da59abd2dafb8e86771f2d9c169b52
Unknown <gm21007@ues.edu.sv>
2023-05-31 14:07:00 -0600

    Creacion de vistas y urls
    Cree las vistas y urls para que todo lo que agregue se pueda visualizar

--- dhp_proyecto/appdiscapacitados/urls.py
+++ dhp_proyecto/appdiscapacitados/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import *
+
 urlpatterns=[
+    path('',inicio, name='inicio'),
+    path('/graficos', graficos, name='graficos'),
+    path('/tablas',tablas, name='tablas'),
     path('/resumen',Publicacion,name="publicacion"),
 ]
--- dhp_proyecto/appdiscapacitados/views.py
+++ dhp_proyecto/appdiscapacitados/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def Publicacion(request):
     return render(request,'publicacion.html')
 
-def Graficos(request):
-    return render(request, 'graficos.html')
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+
+def graficos(request):
+    return render(request, 'paginas/graficos.html')
+
+def tablas(request):
+    return render(request,'paginas/tablas.html')
