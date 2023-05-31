e2ccd230140324aa0df76c651e040cf4d257f8ae
Unknown <gm21007@ues.edu.sv>
2023-05-31 15:16:16 -0600

    Correccion de errores
    Correccion de errores de plantilla he implementacion del menu funcional.
    nueva ruta de acceso http://127.0.0.1:8000/appdiscapacitados/

--- dhp_proyecto/appdiscapacitados/urls.py
+++ dhp_proyecto/appdiscapacitados/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('',inicio, name='inicio'),
+    path('/',inicio, name='inicio'),
     path('/graficos', graficos, name='graficos'),
     path('/tablas',tablas, name='tablas'),
     path('/resumen',Publicacion,name="publicacion"),
