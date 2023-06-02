b12db2352ad1e0a832af9f573757e8ff1db5cff2
Unknown <gm21007@ues.edu.sv>
2023-06-02 15:37:27 -0600

    Insercion de datos en tablas y graficos
    Las lineas de codigo agregadas permiten extraer la info de la bd y las presenta en las tablas y en los graficos

--- dhp_proyecto/appdiscapacitados/views.py
+++ dhp_proyecto/appdiscapacitados/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import CondicionEconomicaPorDiscapacidad
+from .models import CondicionActividadEconomica
+from .models import DiscapacitadoPorGenero
+from .models import ParticipacionPorDiscapacidad
 
 # Create your views here.
 def Publicacion(request):
@@ -8,7 +12,22 @@ def inicio(request):
     return render(request, 'paginas/inicio.html')
 
 def graficos(request):
-    return render(request, 'paginas/graficos.html')
+    gra1 = DiscapacitadoPorGenero.objects.all()
+    nombres1 = ""
+    dat1 = ""
+    for elemento in gra1:
+        nombres1 = nombres1 + "'" + elemento.estado_discapacitado + "',"
+        dat1 = dat1 + elemento.porcentaje_discapacidad + ","
+
+    gra2 = ParticipacionPorDiscapacidad.objects.all()
+    nombres2 = ""
+    dat2 = ""
+    for elemento in gra2:
+        nombres2 = nombres2 + "'"+ elemento.tipo_discapacidad + "',"
+        dat2 = dat2 + elemento.porcentaje_participacion +","
+    return render(request, 'paginas/graficos.html',{"gra1":gra1,"nombres1":nombres1,"dat1":dat1,"gra2":gra2,"nombres2":nombres2,"dat2":dat2})
 
 def tablas(request):
-    return render(request,'paginas/tablas.html')
+    datos1 = CondicionEconomicaPorDiscapacidad.objects.all()
+    datos2 = CondicionActividadEconomica.objects.all()
+    return render(request,'paginas/tablas.html',{'datos1':datos1,'datos2':datos2})
